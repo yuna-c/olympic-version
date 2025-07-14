5d49d561c0e12126901e12bf9e1824decaa4e0a9
yuna <yuna744@gmail.com>
2025-07-14 18:21:04 +0900

    📝 fix: provider 위치 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
 import Form from './components/form/Form';
 import Sort from './components/form/Sort';
 import Table from './components/table/Table';
-import Providers from './providers/Providers';
 import './App.css';
 
 function App() {
   return (
     <div id="wrap">
       <h1>Context : 올림픽 매달 트래커</h1>
-      <Providers>
-        <Form />
-        <Sort />
-        <Table />
-      </Providers>
+      <Form />
+      <Sort />
+      <Table />
     </div>
   );
 }
